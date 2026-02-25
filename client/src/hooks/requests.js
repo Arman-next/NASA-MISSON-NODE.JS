f98648561e4e7245c9ece87710ab493c4fd26e2a
Arman Khan <arman276khan@gmail.com>
2026-02-25 22:18:38 +0530

    first commit

---
+++ client/src/hooks/requests.js
@@ -0,0 +1,25 @@
+// Load planets and return as JSON.
+async function httpGetPlanets() {
+  // TODO:Once API is ready
+  // load planets and return as JSON
+}
+
+// Load launches, sort by flight number, and return as JSON.
+async function httpGetLaunches() {
+  // TODO:Once API is ready
+  // load launces, sort by flight no and return as JSON
+}
+
+// Submit given launch data to launch system.
+async function httpSubmitLaunch(launch) {
+  // TODO:Once API is ready
+  // submit given launch data to launch system
+}
+
+// Delete launch with given ID.
+async function httpAbortLaunch(id) {
+  // TODO:Once API is ready
+  // Delete launch with given id
+}
+
+export { httpGetPlanets, httpGetLaunches, httpSubmitLaunch, httpAbortLaunch };
